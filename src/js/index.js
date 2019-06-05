158c0ad5d27572bd6d1bf0756e9afb226941c1db
Doomka <do-omer@ya.ru>
2019-06-05 09:17:37 +0400

    sliding main_left

--- src/js/index.js
+++ src/js/index.js
@@ -16,8 +16,8 @@ document.addEventListener('DOMContentLoaded', (e)=> {
 			simulateTouch: false,
 			// autoHeight: true,
 			navigation: {
-			  nextEl: '.other_products-slider .controls .next',
-			  prevEl: '.other_products-slider .controls .prev',
+				nextEl: '.other_products-slider .controls .next',
+				prevEl: '.other_products-slider .controls .prev',
 			},
 			keyboard: {
 				enabled: true,
@@ -35,8 +35,8 @@ document.addEventListener('DOMContentLoaded', (e)=> {
 			// spaceBetween: 0,
 			// autoHeight: true,
 			navigation: {
-			  nextEl: '.reviews .slider .controls .next',
-			  prevEl: '.reviews .slider .controls .prev',
+				nextEl: '.reviews .slider .controls .next',
+				prevEl: '.reviews .slider .controls .prev',
 			},
 			keyboard: {
 				enabled: true,
@@ -45,4 +45,62 @@ document.addEventListener('DOMContentLoaded', (e)=> {
 		})
 	}
 	
+	// if (document.querySelector('.main_left')) {
+	// 	let swiper = new Swiper('.main .container', {
+	// 		slidesPerView: 'auto',
+	// 		freemode: true,
+	// 		// virtualTranslate: true,
+	// 		// simulateTouch: false,
+	// 	})
+	// }
+	
+	
+	
+	document.querySelector('.main-content').addEventListener('touchstart', handleTouchStart, false);
+	document.querySelector('.main-content').addEventListener('touchmove', handleTouchMove, false);
+	
+	var xDown = null;
+	var yDown = null;
+	
+	function getTouches(evt) {
+		return evt.touches ||             // browser API
+		evt.originalEvent.touches; // jQuery
+	}
+	
+	function handleTouchStart(evt) {
+		const firstTouch = getTouches(evt)[0];
+		xDown = firstTouch.clientX;
+		yDown = firstTouch.clientY;
+	};
+	
+	function handleTouchMove(evt) {
+		if ( ! xDown || ! yDown ) {
+			return;
+		}
+		
+		var xUp = evt.touches[0].clientX;
+		var yUp = evt.touches[0].clientY;
+		
+		var xDiff = xDown - xUp;
+		var yDiff = yDown - yUp;
+		
+		if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+			if ( xDiff > 0 ) {
+				document.querySelector('.main-content .main_left').classList.remove('active')
+				/* left swipe */
+			} else {
+				document.querySelector('.main-content .main_left').classList.add('active')
+				/* right swipe */
+			}
+		} else {
+			if ( yDiff > 0 ) {
+				/* up swipe */
+			} else {
+				/* down swipe */
+			}
+		}
+		/* reset values */
+		xDown = null;
+		yDown = null;
+	}
 })
